ee6436ba84711dd62653ae59301b2b36861a2f8a
ruandias <ruan_ndias@hotmail.com>
2021-01-09 23:59:21 -0300

    added API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiKey: '1a788676b927fd9d836e736fd6e92e25',
+  apiKey: 'eaa29d7f66143faff31dbf2708f841a1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: '1a788676b927fd9d836e736fd6e92e25',
+  apiKey: 'eaa29d7f66143faff31dbf2708f841a1',
 };
 
 /*
